a3ab4442aadfedbca8f784bb42dff7d7ec8e173b
Sergio Loza <sergiolozav@gmail.com>
2024-10-17 17:40:25 -0400

    feat: added custom sidebar button

---
+++ InvoiceApp/Forms/ButtonSidebar.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace InvoiceApp.Forms
+{
+    partial class ButtonSidebar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ButtonSidebar));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 0);
+            pictureBox1.MinimumSize = new Size(0, 24);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(25, 45);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Dock = DockStyle.Left;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 0);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Padding = new Padding(12, 8, 0, 8);
+            label1.Size = new Size(67, 39);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // ButtonSidebar
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "ButtonSidebar";
+            Padding = new Padding(12, 0, 12, 0);
+            Size = new Size(300, 45);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private Label label1;
+    }
+}
---
+++ InvoiceApp/Forms/ButtonSidebar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Button = System.Windows.Forms.Button;
+
+namespace InvoiceApp.Forms
+{
+    public partial class ButtonSidebar : UserControl
+    {
+        public ButtonSidebar()
+        {
+            InitializeComponent();
+        }
+
+        [Category("CustomOptions")]
+        public string OptionName
+        {
+            get { return label1.Text; }
+            set { label1.Text = value; }
+        }
+
+        [Category("CustomOptions")]
+        public Image OptionImage
+        {
+            get { return pictureBox1.Image; }
+            set { pictureBox1.Image = value; }
+        }
+
+        [Category("CustomOptions")]
+        public Color OptionBackgroundColor
+        {
+            get { return BackColor; }
+            set { BackColor = value; }
+        }
+    }
+}
