b4c6d91ae5a164b9b47513bf04a0fc24c0015c3f
Sergio Loza <sergiolozav@gmail.com>
2024-10-18 14:40:55 -0400

    refactor: added to remove warning

--- InvoiceApp/Models/Item.cs
+++ InvoiceApp/Models/Item.cs
@@ -2,7 +2,7 @@
 {
     public class Item
     {
-        public int Description { get; set; }
+        public required string Description { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
     }
