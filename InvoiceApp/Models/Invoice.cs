94a77871d32b0d411000e23001a54922b11026c7
Sergio Loza <sergiolozav@gmail.com>
2024-10-18 13:03:32 -0400

    feat: Created models for Invoice and Item

---
+++ InvoiceApp/Models/Invoice.cs
@@ -0,0 +1,12 @@
+﻿namespace InvoiceApp.Models
+{
+    public class Invoice
+    {
+        required public string Cuf { get; set; }
+        required public string Nit { get; set; }
+        required public string CustomerName { get; set; }
+        required public decimal TotalAmount { get; set; }
+        required public DateTime CreatedAt { get; set; }
+        required public int Status { get; set; }
+    }
+}
---
+++ InvoiceApp/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace InvoiceApp.Models
+{
+    public class Item
+    {
+        public int Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
