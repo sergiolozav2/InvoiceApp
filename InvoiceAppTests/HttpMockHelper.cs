c900b25e6290a8881b49633c32100922701b3cad
Sergio Loza <sergiolozav@gmail.com>
2024-10-18 13:14:55 -0400

    test: added http mock helper

---
+++ InvoiceAppTests/HttpMockHelper.cs
@@ -0,0 +1,37 @@
+﻿using Moq.Protected;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceAppTests
+{
+    internal class HttpMockHelper
+    {
+        public static HttpClient CreateClient(object mockedResponse, HttpStatusCode statusCode)
+        {
+            var httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+
+            HttpResponseMessage httpResponseMessage = new()
+            {
+                Content = JsonContent.Create(mockedResponse),
+                StatusCode = statusCode
+            };
+            var setupMock = httpMessageHandlerMock
+           .Protected() // <= this is most important part that it need to setup.
+           .Setup<Task<HttpResponseMessage>>(
+               "SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
+
+            setupMock.ReturnsAsync(httpResponseMessage);
+
+            var httpClient = new HttpClient(httpMessageHandlerMock.Object);
+
+            return httpClient;
+        }
+
+    }
+}
